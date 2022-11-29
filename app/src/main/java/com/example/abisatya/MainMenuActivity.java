3abc3b4b48194bc9678cd54d16f90a49029a3fbe
taufiksatrian <72427297+taufiksatrian@users.noreply.github.com>
2022-11-29 21:00:33 +0700

    "Progress Isya"

--- app/src/main/java/com/example/abisatya/MainMenuActivity.java
+++ app/src/main/java/com/example/abisatya/MainMenuActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainMenuActivity extends AppCompatActivity {
     private ConstraintLayout lCatatan, lSisiAbisatya;
@@ -34,10 +39,18 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
 
+//        Uri imageUri = Uri.parse(getIntent().getExtras().getString("PROFIL"));
+//        try {
+//            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+//            ibtnProfil.setImageBitmap(bitmap);
+//        } catch (IOException e) {
+//            Toast.makeText(this, "Failed load images", Toast.LENGTH_SHORT).show();
+//        }
+
         lCatatan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent catat = new Intent(MainMenuActivity.this, TambahCatatanActivity.class);
+                Intent catat = new Intent(MainMenuActivity.this, CatatanActivity.class);
                 startActivity(catat);
             }
         });
--- app/src/main/java/com/example/abisatya/ProfilActivity.java
+++ app/src/main/java/com/example/abisatya/ProfilActivity.java
@@ -1,15 +1,30 @@
 package com.example.abisatya;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class ProfilActivity extends AppCompatActivity {
+    private static final int PROFIL_REQUEST_CODE = 1;
+
+    private String sTentang, sNama, sTelpon, sEmail;
+    private ImageView ibtnProfil, imgProfil, iProfil;
+    private boolean change_img_profil = false;
+    private String uriString = null;
+
     private ImageView ibtnBack;
     private Button btnSimpan;
     private EditText etTentang, etNama, etTelpon, etEmaill;
@@ -20,6 +35,16 @@ public class ProfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
 
+        ibtnProfil = findViewById(R.id.btnProfil);
+        imgProfil = findViewById(R.id.imgProfil);
+
+        ibtnProfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), PROFIL_REQUEST_CODE);
+            }
+        });
+
         etTentang = findViewById(R.id.etTentang);
         etNama = findViewById(R.id.etNama);
         etTelpon = findViewById(R.id.etNoTelpon);
@@ -29,10 +54,13 @@ public class ProfilActivity extends AppCompatActivity {
         btnSimpan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent catatan = new Intent(ProfilActivity.this, MainMenuActivity.class);
+                Intent catatan = new Intent(ProfilActivity.this, ProfilActivity.class);
+                catatan.putExtra("PROFIL", uriString);
+                catatan.putExtra("TENTANG", sTentang);
                 startActivity(catatan);
             }
         });
+        etTentang.setText(sTentang);
 
         ibtnBack = findViewById(R.id.btnBack);
         ibtnBack.setOnClickListener(new View.OnClickListener() {
@@ -42,5 +70,26 @@ public class ProfilActivity extends AppCompatActivity {
                 startActivity(catatan);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_CANCELED){
+            Toast.makeText(this, "Canceled pick images", Toast.LENGTH_SHORT).show();
+        } else if (requestCode == PROFIL_REQUEST_CODE){
+            if (data != null){
+                try {
+                    Uri imageUri = data.getData();
+                    uriString = imageUri.toString();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    iProfil.setImageBitmap(bitmap);
+                } catch (IOException e){
+                    Toast.makeText(this, "Failed load images", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
